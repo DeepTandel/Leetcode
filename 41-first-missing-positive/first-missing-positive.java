5076df2bb382431f46ea8c1fac8a836833c4d29e
Deep <118455526+DeepTandel@users.noreply.github.com>
2024-05-08 00:05:54 +0530

    Time: 1 ms (96.14%) | Memory: 57.2 MB (25.46%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for(int i=0; i<n; i++) {
+            if(nums[i] <=0 || nums[i]>=n+1) {
+                nums[i] = n+1;
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            int element = Math.abs(nums[i]);
+            if(element == n+1) {
+                continue;
+            }
+            int seat = element - 1;
+            if(nums[seat] > 0) {
+                nums[seat] = -nums[seat];
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            if(nums[i] > 0) {
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+}
